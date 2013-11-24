43ff101925b782a18db2d137e33cb3d9e7e5cf4f
jqcui <jqcui@student.cs.uwaterloo.ca>
2013-11-23 21:44:14 -0500

    pte function now compilable

--- os161-1.99/kern/vm/pt.c
+++ os161-1.99/kern/vm/pt.c
@@ -1,60 +1,62 @@
 #include <pt.h>
+#include <mips/vm.h>
 
 void initPT(struct pt * pageTable) {
+	(void) pageTable;
 	//find out how many pages the proc needs
-	
+
 	//create a pt
 
 	//for every page that it needs, create a pte and init appropriate fields
 }
 
-paddr_t getPTE(vaddr_t addr) {
-	
+paddr_t getPTE(struct pt* pageTable, vaddr_t addr) {
+
+	//text segment
 	vaddr_t textBegin;
 	vaddr_t textEnd;
+
+	//data segment
 	vaddr_t dataBegin;
 	vaddr_t dataEnd;
-	
-	int vpn;	
+
+	int vpn;
 	//initialize the above variables
 
 	//boundary checking?
 
 	//not in either segments
-	if(!((textBegin <= addr && addr <= textEnd) || (dataBegin <= addr && addr <= dataEnd))) {
+	if (!((textBegin <= addr && addr <= textEnd)
+			|| (dataBegin <= addr && addr <= dataEnd))) {
 		//kill process
-		return (paddr_t)-1;
+		return (paddr_t) -1;
 	}
-	//in text segment
-	else if(textBegin <= addr && addr <= textEnd) {
-		vpn = ((addr - textBegin) & 0xfffff000)/ PAGE_SIZE;
 
-		if(text[vpn]->valid) {
+	//in text segment
+	else if (textBegin <= addr && addr <= textEnd) {
+		vpn = ((addr - textBegin) & PAGE_FRAME) / PAGE_SIZE;
+
+		if (pageTable->text[vpn]->valid) {
 			//add mapping to tlb, evict victim if necessary
 			//reexecute instruction
-			return text[vpn]->paddr;
-		}
-		else {
+			return pageTable->text[vpn]->paddr;
+		} else {
 			//page fault
 			//get from swapfile or elf file
 		}
 	}
 	//in data segment
-	//else if (dataBegin <= addr && addr <= dataEnd) {
-	else {
-		vpn = ((addr - dataBegin) & 0xfffff000)/ PAGE_SIZE;
+	else if (dataBegin <= addr && addr <= dataEnd) {
+		vpn = ((addr - dataBegin) & PAGE_FRAME) / PAGE_SIZE;
 
-		if(data[vpn]->valid) {
+		if (pageTable->data[vpn]->valid) {
 			//add mapping to tlb, evict victim if necessary
 			//reexecute instruction
-			return data[vpn]->paddr;
-		}
-		else {
+			return pageTable->data[vpn]->paddr;
+		} else {
 			//page fault
 		}
 	}
 
-	//for every pte, check if page number * pagesize <= addr <= page number +1 * pagesize
-	(void) addr;
 	return 0; //change this later
 }
