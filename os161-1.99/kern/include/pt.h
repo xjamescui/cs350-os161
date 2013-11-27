d5a4168b335371888281714dc79ade2c8a05f75a
Davis Wang <davisxwang@gmail.com>
2013-11-26 21:42:45 -0500

    added page replacement algo (random) as well as small changes to other pt methods

--- os161-1.99/kern/include/pt.h
+++ os161-1.99/kern/include/pt.h
@@ -10,8 +10,11 @@ struct pte {
 
 struct pt {
 	//have two "mini" page tables for each segment
+	unsigned int numTextPages;
+	unsigned int numDataPages;
 	struct pte** text;
 	struct pte** data;
+	struct pte** stack;
 };
 
 void initPT(struct pt * pgTable, int numTextPages, int numDataPages);
@@ -19,11 +22,16 @@ void initPT(struct pt * pgTable, int numTextPages, int numDataPages);
 struct pte * getPTE(struct pt * pgTable, vaddr_t addr);
 
 // this function is called when there is a page fault, and we need to get the corresponding page from swapfile or ELF file
-// the boolean is 1 if vaddr is in text segment, 0 if it's in the only other segment (data)
+// the unsigned short int is 0 if text seg, 1 if data seg, and 2 if stack seg.
 // segBegin thus corresponds with the beginning vaddr of the segment define by inText
-struct pte * loadPTE(struct pt * pgTable, vaddr_t vaddr, bool inText,
+struct pte * loadPTE(struct pt * pgTable, vaddr_t vaddr, unsigned short int segmentNum,
 		vaddr_t segBegin);
 
+
+//We use a random page replacement algorithm, as it is actually faster than FIFO in practise.
+//it is also by far the simplest replacement algorithm to use as we needn't keep track of the order of pages
+//inserted into the page table like FIFO needs.
+int getVictimVPN(struct pt * pgTable, unsigned short int segmentNum);
 /* vaddr_t to paddr_t */
 
 /*
--- os161-1.99/kern/vm/pt.c
+++ os161-1.99/kern/vm/pt.c
@@ -13,11 +13,14 @@
 #include <current.h>
 #include <vfs.h>
 
+#define DUMBVM_STACKPAGES    12
+
 void initPT(struct pt * pgTable, int numTextPages, int numDataPages) {
 
 //	const int numStackPages = 12;
 //	(void) numStackPages;
-
+	pgTable->numTextPages = numTextPages;
+	pgTable->numDataPages = numDataPages;
 	pgTable->text = kmalloc(numTextPages * sizeof(struct pte*));
 	pgTable->data = kmalloc(numDataPages * sizeof(struct pte*));
 
@@ -57,6 +60,10 @@ struct pte * getPTE(struct pt* pgTable, vaddr_t addr) {
 	//data segment
 	vaddr_t dataBegin = curproc_getas()->as_vbase_data;
 	vaddr_t dataEnd = dataBegin + curproc_getas()->as_npages_data * PAGE_SIZE;
+
+	vaddr_t stackBegin; //this is also constant, i think
+	vaddr_t stackEnd; //this is constant, i think
+	(void)stackEnd; //do we need stackEnd?
 
 	KASSERT(dataEnd > dataBegin);
 
@@ -104,12 +111,27 @@ struct pte * getPTE(struct pt* pgTable, vaddr_t addr) {
 			return loadPTE(pgTable, addr, 0, dataBegin);
 		}
 	}
+	//in stack segment
+	else {
+		vpn = ((addr - stackBegin) & PAGE_FRAME) / PAGE_SIZE;
+		entry = pgTable->stack[vpn];
+
+		if (entry->valid) {
+			//add mapping to tlb, evict victim if necessary
+			//reexecute instruction
+			DEBUG(DB_A3, "addr is in stack segment\n");
+			return entry;
+		} else {
+			DEBUG(DB_A3, "page fault: addr is in stack segment \n");
+			return loadPTE(pgTable, addr, 2, dataBegin);
+		}
+	}
 
 	// return EFAULT;
 	return NULL;
 }
 
-struct pte * loadPTE(struct pt * pgTable, vaddr_t vaddr, bool inText,
+struct pte * loadPTE(struct pt * pgTable, vaddr_t vaddr, unsigned short int segmentNum,
 		vaddr_t segBegin) {
 	//load page based on swapfile or ELF file
  struct vnode *v;
@@ -134,23 +156,70 @@ struct pte * loadPTE(struct pt * pgTable, vaddr_t vaddr, bool inText,
 	//using cm_alloc_pages
 
 	//we only need one page
-	paddr_t paddr = cm_alloc_pages(1); 
+	//paddr_t paddr = cm_alloc_pages(1); 
+ 	paddr_t paddr;
+	long allocPageResult = cm_alloc_pages(1);
+	if(allocPageResult > 0) {
+		paddr = (paddr_t)allocPageResult;
+		//ALERT: there may be a bug here
+	}
     //Register this physical page in the core map
 	//copy paddr over to page table with info from ELF/SWAP file
 	
 	int vpn = ((vaddr - segBegin) & PAGE_FRAME) / PAGE_SIZE;
 
-	if (inText) {
+	//no more physical ram available
+	if(allocPageResult == -1) {
+		//need to check if all pages in page table are valid
+		//if not we use the non valid page
+		//if so, we need to swap out a page in pagetable
+		//then update coremap
+
+		//NOTE: the swapping 
+	}
+
+	if (segmentNum == 0) {
 		pgTable->text[vpn]->paddr = paddr;
 
 		return pgTable->text[vpn];
-	} else {
+	} 
+	else if (segmentNum == 1) {
 		pgTable->data[vpn]->paddr = paddr;
 
 		return pgTable->text[vpn];
+	}
+	else if(segmentNum == 2) {
+		pgTable->stack[vpn]->paddr = paddr;
+		return pgTable->text[vpn];
+	}
+	else {
+		//something wrong!
+		return NULL;
 	}
 
 	//something has gone wrong!
 	// return EFAULT;
 	return NULL;
+}
+
+//We use a random page replacement algorithm, as it is actually faster than FIFO in practise.
+//it is also by far the simplest replacement algorithm to use as we needn't keep track of the order of pages
+//inserted into the page table like FIFO needs.
+//NOTE page replacement will  
+int getVictimVPN(struct pt * pgTable, unsigned short int segmentNum) {
+	//JAMES, PLEASE DEFINE MACROS FOR THESE
+	//text segment = 0, data = 1, stack = 2
+	if(segmentNum == 0) {
+		return random() % pgTable->numTextPages;
+	}
+	else if(segmentNum == 1) {
+		return random() % pgTable->numDataPages;
+	}
+	else if (segmentNum == 2) {
+		return random() % DUMBVM_STACKPAGES;
+	}
+	else {
+		//we shouldn't get here
+		return -1;
+	}
 }
